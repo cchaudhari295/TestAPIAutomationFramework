ac64da8f07253e20aad2e4a1784dff50ea679550
Chaitali <crane@symplr.com>
2025-10-19 17:13:06 +0530

    Introduced Enums for handling the codes for the Create Job API Test. Reduced Magic number issue in the test script

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -7,7 +7,14 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warranty_Status;
 import com.api.request.model.CreateJobPayload;
 import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
@@ -25,13 +32,13 @@ public class CreateJobAPITest {
 public void createJobAPI() {
 	Customer customer = new Customer("Chats", "C", "9876543212", "", "testCreateJobAPI@gmail.com", "");
 	CustomerAddress customerAddress = new CustomerAddress("A123","Nexus","Whitefield","","","560042","India","Karanataka");
-	CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "10721231924144", "10721231924144", "10721231924144", getTimeWithDaysAgo(10), 1, 1);
-	Problems problems = new Problems(1, "Battery Issue");
+	CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "10721231923141", "10721231924141", "10721231924141", getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
+	Problems problems = new Problems(Problem.OVERHEATING.getCode(), "Battery Issue");
 	List<Problems> problemsArray = new ArrayList<Problems>();
 	
 	problemsArray.add(problems);
 	
-	CreateJobPayload createJobPayload=new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemsArray);
+	CreateJobPayload createJobPayload=new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warranty_Status.IN_WARRANTY.getCode(), OEM.APPLE.getCode(), customer, customerAddress, customerProduct, problemsArray);
 	
 	RestAssured.given()
 		.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
@@ -41,7 +48,7 @@ public void createJobAPI() {
 			 .spec(SpecUtil.responseSpec_OK())
 			 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
 			 .body("message",equalTo("Job created successfully. "))
-			 .body("mst_service_location_id", equalTo(1))
+			 .body("mst_service_location_id", equalTo(ServiceLocation.SERVICE_LOCATION_A.getCode()))
 			 .body("job_number",startsWith("JOB_"));
 }
 	
