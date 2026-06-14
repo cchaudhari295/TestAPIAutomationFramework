af1fd784e3b3adcf7dee3a28e12808c374f67eaf
Chaitali <chaitali1.chaudhari@gmail.com>
2026-06-14 14:50:43 +0530

    Added Allure Annotations for Better Reporting
    Introduced Epic,Feature,Story,Description, Severity in the tests
    com.api.tests

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -7,10 +7,24 @@ import static io.restassured.RestAssured.given;
 
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+@Epic("User Management")
+@Feature("User Details")
 
 public class UserDetailsAPITest {
 	
+
+	@Story("UserDetails should be shown")
+	@Description("Verify if the Userdetials API response is shown correctly")
+	@Severity(SeverityLevel.CRITICAL)
 	@Test(description="Verify if the user details api response is shown correctly",groups= {"api","smoke","regression"})
 	public void userDetailsAPITest() {
 				
